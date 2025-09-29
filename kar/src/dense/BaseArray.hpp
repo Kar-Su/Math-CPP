1a51555e265dce25760042b7a4f6e8930fdc8560
Helmi <rumahhelmi@gmail.com>
2025-09-29 21:58:50 +0800

    feat(BaseArray): Add lexicographic function
    lexicographic:
    Turn N-dims index to row-major index
    
    -Data stored with row-major.
    
    Signed-off-by: Helmi <rumahhelmi@gmail.com>

--- kar/src/dense/BaseArray.hpp
+++ kar/src/dense/BaseArray.hpp
@@ -2,41 +2,61 @@
 #define BASEARRAY_HPP
 
 #include <array>
+#include <cassert>
 #include <cstddef>
 
 namespace kar {
 namespace container {
 
 template <typename TYPE, std::size_t... DIMS_SEQ> class Array {
+public:
   static constexpr std::size_t rank = sizeof...(DIMS_SEQ);
   static constexpr std::size_t size = (std::size_t(1) * ... * DIMS_SEQ);
 
   static constexpr std::array<TYPE, rank> shape{DIMS_SEQ...};
   static constexpr std::array<TYPE, rank> stride_element = []() {
     std::array<TYPE, rank> s{};
-    if constexpr (rank == 0) {
+    if constexpr (rank == 0)
       return s;
-    }
+
     s[rank - 1] = 1;
-    for (std::size_t i = rank - 1; i-- > 0;) {
+    for (std::size_t i = rank - 1; i-- > 0;)
       s[i] = s[i + 1] * shape[i + 1];
-    }
+
     return s;
   };
 
   static constexpr std::array<TYPE, rank> stride_byte = []() {
     std::size_t byte = sizeof(TYPE);
     std::array<TYPE, rank> s{};
-    if constexpr (rank == 0) {
+    if constexpr (rank == 0)
       return s;
-    }
 
     s[rank - 1] = byte;
-    for (std::size_t i = rank - 1; i-- < rank;) {
+    for (std::size_t i = rank - 1; i-- < rank;)
       s[i] = shape[i - 1] * s[i - 1];
-    }
+
     return s;
   };
+
+private:
+  std::array<TYPE, size> data;
+
+  static constexpr std::size_t
+  lexicographic(std::array<std::size_t, rank> idx) {
+    if constexpr (rank == 0)
+      return 0;
+
+#ifndef NDEBUG
+    for (std::size_t i = 0; i < rank; ++i)
+      assert(idx[i] < shape[i]);
+#endif // !NDEBUG
+    std::size_t offside = 0;
+    for (std::size_t i = 0; i < rank; ++i)
+      offside += idx[i] * stride_element[i];
+
+    return offside;
+  }
 };
 
 } // namespace container
